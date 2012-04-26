1b7e7b1264ad8e23354af5b5df4e39d1ee978d39
Thiago Furtado <thiagofurtado17@gmail.com>
2012-04-25 20:14:24 -0400

    general commit

--- src/com/example/android/actionbarcompat/MainActivity.java
+++ src/com/example/android/actionbarcompat/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.android.actionbarcompat;
 
 import com.google.android.maps.MapView;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -8,34 +13,68 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
-    private MapView mapView;
+	private MapView mapView;
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-        mapView = (MapView) findViewById(R.id.mapView);
-        mapView.setBuiltInZoomControls(true);
-         
-    }
+		mapView = (MapView) findViewById(R.id.mapView);
+		mapView.setBuiltInZoomControls(true);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+		setUserLocationListener();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            
-            case R.id.menu_filter:
-                Toast.makeText(this, "Tapped", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private void setUserLocationListener() {
+		// Acquire a reference to the system Location Manager
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+		// Define a listener that responds to location updates
+		LocationListener locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location provider.
+				setUserMarker(location);
+			}
+
+			private void setUserMarker(Location location) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+
+			}
+
+			public void onProviderEnabled(String provider) {
+
+			}
+
+			public void onProviderDisabled(String provider) {
+
+			}
+		};
+
+		// Register the listener with the Location Manager to receive location updates
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+
+		case R.id.menu_filter:
+			Toast.makeText(this, "Tapped", Toast.LENGTH_SHORT).show();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	@Override
 	protected boolean isRouteDisplayed() {
